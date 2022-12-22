6626b201c836632a32db9f2906e976a1fc14c970
Konstantin Losev <konstantin.losev@gcore.com>
2022-12-22 19:45:50 +0400

    Add .bashrc

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -3,7 +3,7 @@ vim.cmd([[packadd packer.nvim]])
 return require("packer").startup(function()
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
-	use("nathom/filetype.nvim")
+	-- use("nathom/filetype.nvim")
 	use({ "volvofixthis/rest.nvim", branch = "fixes" })
 	use("wesleimp/stylua.nvim")
 	use("neovim/nvim-lspconfig")
