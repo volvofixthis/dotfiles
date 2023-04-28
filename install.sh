6b8906996cc10eea001402d23790ab84e082ad3d
Konstantin Losev <konstantin.losev@gcore.com>
2023-04-28 09:56:48 +0000

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 mkdir -p ~/bin
-mkdir -p ~/.config/systemd/user
+mkdir -p ~/.config/systemd/user/default.target.wants
 mkdir -p ~/.config/environment.d/
 
-stow bash
 stow bin
 stow isort
 stow systemd
