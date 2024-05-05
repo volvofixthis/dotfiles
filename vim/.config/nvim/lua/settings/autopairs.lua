c5ee8b9dbb58721058478467a4a8feb037f6958b
Konstantin Losev <konstantin.losev@gcore.com>
2024-05-05 17:14:30 +0400

    Updated settings for autorpairs

--- vim/.config/nvim/lua/settings/autopairs.lua
+++ vim/.config/nvim/lua/settings/autopairs.lua
@@ -1,4 +1,3 @@
-local remap = vim.api.nvim_set_keymap
 local npairs = require("nvim-autopairs")
 
 npairs.setup({
@@ -9,12 +8,9 @@ npairs.setup({
         offset = 0, -- Offset from pattern match
         end_key = "$",
         keys = "qwertyuiopzxcvbnmasdfghjkl",
+        cursor_pos_before = true,
         check_comma = true,
         highlight = "Search",
         highlight_grey = "Comment",
     },
 })
-
--- these mappings are coq recommended mappings unrelated to nvim-autopairs
-remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
