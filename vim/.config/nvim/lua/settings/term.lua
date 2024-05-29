d94a8ad9f1d756cd0a3beefe7384d04e7f24e53e
Konstantin Losev <konstantin.losev@gcore.com>
2024-05-30 03:14:54 +0400

    Fixed terminal

--- vim/.config/nvim/lua/settings/term.lua
+++ vim/.config/nvim/lua/settings/term.lua
@@ -2,37 +2,38 @@ local function gen_callback(layout)
     return function()
         local path = get_current_folder_path()
         vim.cmd(layout)
-        vim.cmd("term cd " .. path .. " && $SHELL && alias vim='nvr --remote-tab'")
+        vim.cmd("term cd " .. path .. " && $SHELL")
         vim.cmd("file term" .. vim.fn.bufnr())
         vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", "", {
             noremap = true,
             callback = function()
-                vim.cmd("stopinsert")
+                vim.cmd.stopinsert()
             end,
         })
         vim.api.nvim_buf_set_keymap(0, "t", "<C-q>", "", {
             noremap = true,
             callback = function()
-                vim.cmd("q!")
+                vim.cmd("sil! bp! | bd!#")
             end,
         })
         vim.api.nvim_buf_set_keymap(0, "n", "<C-q>", "", {
             noremap = true,
             callback = function()
-                vim.cmd("q!")
+                vim.cmd("sil! bp! | bd!#")
             end,
         })
-        vim.cmd("startinsert")
+        vim.cmd.startinsert()
     end
 end
 
 local new_term_tab = gen_callback("tabnew")
+local new_right = gen_callback("wincmd l")
 local new_term_split = gen_callback("split")
 local new_term_vsplit = gen_callback("vsplit")
 
 vim.api.nvim_set_keymap("n", "<C-t>", "", {
     noremap = true,
-    callback = new_term_tab,
+    callback = new_right,
 })
 vim.api.nvim_set_keymap("n", "<Leader>th", "", {
     noremap = true,
