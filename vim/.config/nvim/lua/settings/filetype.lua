19252a9dc0b66b5be74cd16e1ae2264b923bd513
Konstantin Losev <konstantin.losev@gcore.com>
2024-09-30 18:43:05 +0400

    Added hcl syntax for terraform rc

--- vim/.config/nvim/lua/settings/filetype.lua
+++ vim/.config/nvim/lua/settings/filetype.lua
@@ -1,3 +1,11 @@
+local hcl_syntax = function()
+    vim.bo.filetype = "hcl"
+    vim.bo.commentstring = '// %s'
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.expandtab = true
+    vim.bo.softtabstop = 2
+end
 require("filetype").setup({
     overrides = {
         extensions = {
@@ -13,18 +21,11 @@ require("filetype").setup({
         function_extensions = {
             ["sql"] = function()
                 vim.bo.filetype = "sql"
-                vim.bo.syntax = "off",
-                vim.keymap.del('i','<left>',{buffer = true})
-                vim.keymap.del('i','<right>',{buffer = true})
+                vim.bo.syntax = "off"
+                vim.keymap.del('i', '<left>', { buffer = true })
+                vim.keymap.del('i', '<right>', { buffer = true })
             end,
-            ["tf"] = function()
-                vim.bo.filetype = "hcl"
-                vim.bo.commentstring = '// %s'
-                vim.opt.tabstop = 2
-                vim.opt.shiftwidth = 2
-                vim.opt.expandtab = true
-                vim.bo.softtabstop = 2
-            end,
+            ["tf"] = hcl_syntax,
             ["gcss"] = function()
                 vim.opt.tabstop = 2
                 vim.opt.shiftwidth = 2
@@ -38,6 +39,9 @@ require("filetype").setup({
                 vim.opt.expandtab = true
                 vim.bo.softtabstop = 2
             end,
+        },
+        function_complex = {
+            [".*.terraformrc"] = hcl_syntax,
         },
         complex = {
             -- Set the filetype of any full filename matching the regex to gitconfig
